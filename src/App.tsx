7a897612c196eeeeb295796c5a0ff56ff2ed8191
Pritam tirpude <pritam.tirpude9601@gmail.com>
2025-11-06 22:34:33 +0530

    project iniitlaized and setuped with shadcn and tanstack router

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl">Heel Media Lens</h1>
+      <Button variant="destructive">Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
